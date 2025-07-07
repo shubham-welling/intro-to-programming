e95cfc51c5fd5684e10607f75c77192763bed0f1
Shubham Welling <Shubham_Welling@progressive.com>
2025-07-07 14:51:29 -0500

    Initial V2

---
+++ week-1/ReferencesSolution/References.Api/Links/LinkController.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace References.Api.Links
+{
+    public class LinkController : ControllerBase
+    {
+        [HttpGet("/links")]
+        public async Task<ActionResult> GetAllLinksAsync(CancellationToken token)
+        {
+            return Ok();
+        }
+    }
+}
